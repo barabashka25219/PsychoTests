9899d9157e401b5f6314801654de06527cda1ede
v.svirskiy <barabashka25219@icloud.com>
2024-03-04 23:35:25 +0300

    Create model "Answer"

--- questions/admin.py
+++ questions/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Answer
+
+class AnswerInline(admin.StackedInline):
+    model = Answer
 
 class AdminQuestion(admin.ModelAdmin):
     fieldsets = [
-        ("Question description", {"fields": ["header", "text"]}),
+        (None, {"fields": ["header", "question_text"]}),
     ]
+
+    inlines = [AnswerInline,]
 
 admin.site.register(Question, AdminQuestion)
--- questions/models.py
+++ questions/models.py
@@ -2,10 +2,16 @@ from django.db import models
 
 class Question(models.Model):
     header = models.CharField(max_length=80)
-    text = models.TextField(max_length=200)
+    question_text = models.TextField(max_length=200)
 
     def __str__(self):
         if len(self.header) > 50:
             return self.header[:50]
         return self.header
+    
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer_text = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.answer_text
