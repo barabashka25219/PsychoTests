1a1b4b7200fa7438a4f1f2c3fd2e417e54cffef1
v.svirskiy <barabashka25219@icloud.com>
2024-03-04 23:12:54 +0300

    Create "Question" model

--- questions/models.py
+++ questions/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    header = models.CharField(max_length=80)
+    text = models.TextField(max_length=200)
+
+    def __str__(self):
+        if len(self.header) > 50:
+            return self.header[:50]
+        return self.header
+
