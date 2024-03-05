f4e5a1294ef28af9f53373d31539818ee3b76544
v.svirskiy <barabashka25219@icloud.com>
2024-03-05 22:02:53 +0300

    Create model "Poll" 1. Create model "AdminPoll" to show this in the Admin panel 2. Link Question model with AdminPoll

--- questions/admin.py
+++ questions/admin.py
@@ -1,14 +1,25 @@
 from django.contrib import admin
-from .models import Question, Answer
+from .models import Question, Answer, Poll
 
 class AnswerInline(admin.StackedInline):
     model = Answer
 
+class QuestionInline(admin.StackedInline):
+    model = Question.poll.through
+
 class AdminQuestion(admin.ModelAdmin):
     fieldsets = [
-        (None, {"fields": ["header", "question_text"]}),
+        (None, {"fields": ["header", "question_text", "poll"]}),
     ]
 
     inlines = [AnswerInline,]
 
+class AdminPoll(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["header", "description_text", "passed_poll_num"]})
+    ]
+
+    inlines = [QuestionInline]
+
 admin.site.register(Question, AdminQuestion)
+admin.site.register(Poll, AdminPoll)
--- questions/models.py
+++ questions/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
+class Poll(models.Model):
+    header = models.CharField(max_length=80)
+    description_text = models.TextField(max_length=1000)
+    passed_poll_num = models.IntegerField()
+
+    def __str__(self):
+        return self.header
+
 class Question(models.Model):
     header = models.CharField(max_length=80)
     question_text = models.TextField(max_length=200)
+    poll = models.ManyToManyField(Poll)
 
     def __str__(self):
         if len(self.header) > 50:
