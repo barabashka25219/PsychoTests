8d4464fcc23391e4eef9e46a452fdcae6441e80a
v.svirskiy <barabashka25219@icloud.com>
2024-03-11 21:45:26 +0300

    Create resource to view a question with answers 1. Change urls in "polls" app 2. Create view QuestionView 3. Create template "question.html" for QuestionView

--- questions/urls.py
+++ questions/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
-from questions.views import IndexView, PollView
+from django.urls import path, include
+from questions.views import IndexView, PollView, QuestionView
 
 app_name = "polls"
 
 urlpatterns = [
     path("", IndexView, name="polls"),
     path("<int:pk>/", PollView, name="poll"),
+    path("question/<int:pk>/", QuestionView, name="question"),
 ]
--- questions/views.py
+++ questions/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Poll
+from .models import Poll, Question
 
 def IndexView(request):
     polls = Poll.objects.all()
@@ -8,8 +8,19 @@ def IndexView(request):
 
 def PollView(request, pk):
     poll = Poll.objects.get(pk=pk)
+    first_question = poll.question_set.get()
     context = {
         "poll": poll,
+        "first_question": first_question,
         "questions_num": poll.question_set.all().count(),
     }
     return render(request, "questions/poll.html", context)
+
+def QuestionView(request, pk):
+    question = Question.objects.get(pk=pk)
+    answers = question.answer_set.all()
+    context = {
+        "question": question,
+        "answers": answers,
+    }
+    return render(request, "questions/question.html", context)
